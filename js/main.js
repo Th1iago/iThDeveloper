d0a75ef243fd8d6f469ce8e17cb8d1b0d5079e6d
thiago <isaurraldethiago4@gmail.com>
2026-09-22 18:46:25 -0300

    se implementa un archivo js de prueba, solo funcional en index

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const nombre = prompt("¿Cuál es tu nombre?");
+
+const nacimientoAnio = parseInt(prompt("¿En qué año naciste?"));
+
+const numeroFavorito = parseInt(prompt("¿Cuál es tu número favorito?"));
+
+alert(`¡Hola, ${nombre}! Tu año de nacimiento es ${nacimientoAnio} y tu número favorito es ${numeroFavorito}.`);

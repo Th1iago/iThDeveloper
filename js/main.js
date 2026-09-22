e6e371bb2ddd5acf4fa678fa1ddeebb5ca38d648
thiago <isaurraldethiago4@gmail.com>
2026-09-22 18:50:23 -0300

    se agrega una pequenia operacion matematica

--- js/main.js
+++ js/main.js
@@ -4,4 +4,4 @@ const nacimientoAnio = parseInt(prompt("¿En qué año naciste?"));
 
 const numeroFavorito = parseInt(prompt("¿Cuál es tu número favorito?"));
 
-alert(`¡Hola, ${nombre}! Tu año de nacimiento es ${nacimientoAnio} y tu número favorito es ${numeroFavorito}.`);
+alert(`¡Hola, ${nombre}! Tu año de nacimiento es ${nacimientoAnio}, tu número favorito es ${numeroFavorito}, y tu edad es ${2026 - nacimientoAnio}.`);
